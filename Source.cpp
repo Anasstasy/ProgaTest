b080ba5be22ca886b17f76c7471a2f864cf98924
Anasstasy <nastya.khimich13@gmail.com>
2022-12-08 18:44:29 +0200

    Here I left only line with Hi user.

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <cmath>
+#include <time.h>
 
 using namespace std;
 int main()
 {
-	cout << "Hello, user!";
-	return 0;
+    setlocale(LC_ALL, "ru");
+
+    cout << "Hello, user!" << endl;
+
+    int r1, r2;
+    int max, min;
+    const int n = 12;
+    double a[n];
+    cout << "¬ведите прибыль фирмы:";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\n" "мес€ц " << i + 1 << ": ";
+        cin >> a[i];
+    }
+    cout << "¬ведите желаемое начало диапазона: ";
+    cin >> r1;
+    cout << "¬ведите желаемый конец диапазона: ";
+    cin >> r2;
+    max = min = r1 - 1;
+    for (int i = r1; i < r2; i++)
+    {
+        if (a[i] > a[max])
+        {
+            max = i;
+        }
+        if (a[i] < a[min])
+        {
+            min = i;
+        }
+    }
+    cout << "ћинимальна€ прибыль была в мес€це: " << min + 1 << endl;
+    cout << "ћаксимальна прибыль была в мес€це: " << max + 1 << endl;
 }
